c1a74b1b19da56ec2bb843be6808713aa8fa0667
shibatanaoto <naoto.shibata510@gmail.com>
2025-02-17 02:11:41 +0900

    make generator right format of streaming

--- core/morph/api/handler.py
+++ core/morph/api/handler.py
@@ -81,7 +81,7 @@ async def vm_run_file_stream(
         if first_chunk:
             yield first_chunk
         async for chunk in generator:
-            yield chunk
+            yield f"data: {chunk}\n\n"
 
     return StreamingResponse(
         content=_generate_content(),
