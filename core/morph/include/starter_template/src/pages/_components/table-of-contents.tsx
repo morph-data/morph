0639723ce7aaab805a7bb0103de95573ee8af575
Atsuki Hasegawa <58581243+atsuki44@users.noreply.github.com>
2025-03-21 14:57:06 +0900

    fix: toc style

--- core/morph/include/starter_template/src/pages/_components/table-of-contents.tsx
+++ core/morph/include/starter_template/src/pages/_components/table-of-contents.tsx
@@ -3,7 +3,6 @@ import {
   HoverCardTrigger,
   HoverCardContent,
 } from "@/pages/_components/ui/hover-card";
-import { Card } from "@/pages/_components/ui/card";
 import { Button } from "@/pages/_components/ui/button";
 import { LucideTableOfContents } from "lucide-react";
 import { cn } from "@/pages/_lib/utils";
@@ -24,13 +23,12 @@ export const TableOfContents: React.FC<TocProps> = ({ toc, className }) => {
       <div className={cn("toc text-sm w-full hidden lg:block", className)}>
         <div className="grid gird-cols-1 gap-2.5 w-full">
           {toc.map((entry) => (
-            <a className="x-underline" href={`#${entry.id}`}>
-              <div
-                key={entry.id}
-                className="text-zinc-400 hover:text-zinc-900 cursor-pointer font-normal decoration-zinc-400 decoration-0 line-clamp-2"
-              >
-                <span className="">{entry.value}</span>
-              </div>
+            <a
+              key={entry.id}
+              className="x-underline text-zinc-400 hover:text-zinc-900 font-normal line-clamp-2"
+              href={`#${entry.id}`}
+            >
+              {entry.value}
             </a>
           ))}
         </div>
@@ -43,20 +41,17 @@ export const TableOfContents: React.FC<TocProps> = ({ toc, className }) => {
             </Button>
           </HoverCardTrigger>
           <HoverCardContent className="w-[16rem]">
-            <Card>
-              <div className="grid gird-cols-1 gap-2.5 w-full">
-                {toc.map((entry) => (
-                  <a className="x-underline" href={`#${entry.id}`}>
-                    <div
-                      key={entry.id}
-                      className="text-zinc-400 hover:text-zinc-900 cursor-pointer font-normal decoration-zinc-400 decoration-0 line-clamp-2"
-                    >
-                      {entry.value}
-                    </div>
-                  </a>
-                ))}
-              </div>
-            </Card>
+            <div className="grid gird-cols-1 gap-2.5 w-full">
+              {toc.map((entry) => (
+                <a
+                  key={entry.id}
+                  className="x-underline text-zinc-400 hover:text-zinc-900 font-normal line-clamp-2"
+                  href={`#${entry.id}`}
+                >
+                  {entry.value}
+                </a>
+              ))}
+            </div>
           </HoverCardContent>
         </HoverCard>
       </div>
